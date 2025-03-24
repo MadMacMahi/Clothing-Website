40b1ac35a599f7aafe487abc0e8cc509fbc8a30d
MadMacMahi <superonspeed@gmail.com>
2025-03-24 17:09:07 +0530

    Re-added backend and frontend files

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import'./DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>An e-commerce website an online platform that facilitates buying and selling of products</p>
+                    
+        </div>
+        
+    </div>
+  )
+}
+
+export default DescriptionBox
